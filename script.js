8db64536260ec5bf1e8873809b4e696c5486ad91
Ignatius22 <igee54077@gmail.com>
2020-06-30 12:27:04 +0100

    add loader image to conditionally show on loading the image

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader');
 
 
+
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
 // unsplash Api
-const count = 10;
+const count = 30;
 const apiKey = 'nlHw0zRcjwe_S9Lk9dTOCmvM72w_3BvGxHFLwdVf1EU'
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
+
+// check if image was loaded
+function imageLoaded(){
+	imagesLoaded++
+	console.log(imagesLoaded)
+	if(imagesLoaded === totalImages){
+		ready = true
+		loader.hidden = true
+	}
+}
 
 // helper function to set Attributes on the Dom Element
 function setAttributes(element,attributes){
@@ -18,6 +32,8 @@ function setAttributes(element,attributes){
 
 // create Element for links and photos, add to the dom
 function displayPhoto(){
+	imagesLoaded = 0
+	totalImages = photosArray.length
 	// run function for each object in the array
 	photosArray.forEach((photo)=>{
 		// create an <a> element
@@ -33,6 +49,9 @@ function displayPhoto(){
 			alt: photo.alt_description,
 			title: photo.alt_description
 		})
+
+		// Event Listener to check when each is finished loading
+		img.addEventListener('load',imageLoaded)
 
 		//put <img> inside <a> ,then put both imageContainer Element
 		item.appendChild(img)
@@ -54,9 +73,10 @@ async function getPhotos(){
 
 // check to see if scrolling near bottom, load more photos
 window.addEventListener('scroll', () => {
-	if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+	if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+		ready = false
 		getPhotos()
-		console.log('load more')
+		
 	}
 })
 
